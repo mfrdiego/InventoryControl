0c3921ebab56ff02e2abc5be5456d05a734a5f90
mfrdiego <mfrdiego@hotmail.com>
2022-02-19 16:45:19 -0300

    init project

---
+++ src/main/java/com/diego/InventoryControl/InventoryControlApplication.java
@@ -0,0 +1,13 @@
+package com.diego.InventoryControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diego/InventoryControl/InventoryControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.InventoryControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
